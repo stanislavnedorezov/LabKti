6c49bcd3053f524cc40ff263e46cb49b82441cc3
Nedorezov Stanislav <nedorezov.stanislav2@gmail.com>
2023-11-24 20:00:19 +0300

    Add timer to the home page

--- views.py
+++ views.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
-
+import datetime
 views = Blueprint('views', __name__)
-
 
 @views.route('/')
 def home():
-    return render_template('home.html')
-
+    '''
+    asking server for a current time
+    which actually is quite dumb
+    considering user could easily be
+    in a different timezone.
+    yet it's quite demonstrative
+    '''
+    date_str ='{dt:%B} {dt.day}th {dt.year}'.format(dt=datetime.datetime.now())
+    return render_template('home.html', check=date_str)
 
 @views.route('/success', methods=['GET', 'POST'])
 @login_required
